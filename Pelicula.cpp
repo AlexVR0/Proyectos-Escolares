54a12264c9192e33a7e321de104c343414c93a2d
Alejando Vargas Romo <44343012+AlexVR0@users.noreply.github.com>
2025-02-28 14:52:50 -0600

    Add files via upload

---
+++ Pelicula.cpp
@@ -0,0 +1,45 @@
+#include "Material.h"
+#include "vector"
+#include <iostream>
+using namespace std;
+
+class Pelicula :public Material
+{
+private:
+	int duracion;
+	vector<string> actores;
+	vector<string> directores;
+
+public:
+	Pelicula(string id, string titulo, string genero, string tipo, string region, int cantidad, bool referenciaLimitada, int annio,
+		int duracion,
+		vector<string> actores,
+		vector<string> directores)
+		: Material(id, titulo, genero, tipo, region, cantidad, referenciaLimitada, annio),
+		duracion(duracion),
+		actores(actores),
+		directores(directores)
+	{}
+	// Getters 
+	int getDuracion() const { return duracion; }
+	vector<string> getActores() const { return actores; }
+	vector<string> getDirectores() const { return directores; }
+	// Setters 
+	void setDuracion(const int  duracion) { this->duracion = duracion; }
+	void setActores(const vector<string>& actores) { this->actores = actores; }
+	void setDirectores(const vector<string>& directores) { this->directores = directores; }
+
+	//Override
+	void mostrarInfo() const override {
+		Material::mostrarInfo();
+		cout << "Duracion: " << duracion << endl;
+		for (const auto& actor : actores) {
+			cout << actor << " , ";
+		}
+		cout << endl;
+		for (const auto& director : directores) {
+			cout << director << " , ";
+		}
+		cout << endl;
+	}
+};
